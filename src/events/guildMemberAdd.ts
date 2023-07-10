f31bc28005912c65f299220c649850aa24857487
Ethan Nguyen <sevadus98@gmail.com>
2023-07-10 03:59:33 -0700

    removed console.log

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -4,7 +4,6 @@ import { BotEvent } from '../@types/types';
 const event: BotEvent = {
   name: Events.GuildMemberAdd,
   execute: async (member: GuildMember) => {
-    console.log(member);
     member.setNickname(`${member.user.username}poo`);
     const channel = member.guild.channels.cache.find((ch) => ch.name === 'bot-commands');
     // voice channels do not have a send() method so we need to specify the channel type
